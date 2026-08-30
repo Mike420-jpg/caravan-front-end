103dd598241f0107aa273fae8e4046bb7620da14
Cantis10 <propertyofcantis@gmail.com>
2026-08-31 00:06:55 +0800

    online webhjost

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,23 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <WebView
-      source={{
-        uri: `https://static1.e621.net/data/sample/30/77/30775a57ef42e3ab4be2368e7eaed838.webp`,
-      }}
-    />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{
+          uri: "https://html-files.onrender.com/",
+        }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
   );
 }
-46;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  webview: {
+    flex: 1,
   },
 });
