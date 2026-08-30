8628039a4b15c8afa41766acb8af651bd2b7c093
MichaelLim-QMTFLIM <qmtflim@tip.edu.ph>
2026-08-30 16:40:26 +0800

    Update 7: (30/08/2026)
    Additions & Changes:
    - fixed product cards
    - added search function with search button
    - added ItemNotFound_nobg.png

--- src/app/storepage/storepage.js
+++ src/app/storepage/storepage.js
@@ -5,3 +5,34 @@ function toggleSubcategories(element) {
     subcategories.classList.toggle("hidden");
     arrow.classList.toggle("rotate");
 }
+
+function SearchProducts() {
+    const searchInput = document.getElementById("searchInput").value.toLowerCase();
+    const productCards = document.querySelectorAll(".product-card");
+    const noItemContainer = document.querySelector(".noitem-container");
+
+    let foundProducts = false;
+
+    productCards.forEach(card => {
+        const productName = card.querySelector("#product-name").textContent.toLowerCase();
+        if (productName.includes(searchInput)) {
+            card.style.display = "block";
+            foundProducts = true;
+        } else {
+            card.style.display = "none";
+        }
+    });
+
+    if (!foundProducts) {
+        noItemContainer.style.display = "block";
+    } else {
+        noItemContainer.style.display = "none";
+    }
+}
+
+const form = document.getElementById("Searchbar");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the form from refreshing the page on submit
+    SearchProducts();
+});
