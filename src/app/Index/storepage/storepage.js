1513bc4f761b8287072e3490ef48893faf632545
Mike420-jpg <qmrrnarvarte@tip.edu.ph>
2026-08-30 14:38:41 +0800

    ...

--- src/app/Index/storepage/storepage.js
+++
@@ -1,7 +0,0 @@
-function toggleSubcategories(element) {
-    const subcategories = element.nextElementSibling;
-    const arrow = element.querySelector(".toggle-icon");
-
-    subcategories.classList.toggle("hidden");
-    arrow.classList.toggle("rotate");
-}
